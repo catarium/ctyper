3b967cd170d1ce9583d8135ef71d60bced198ea8
catarium <tartarxan@gmail.com>
2026-04-12 01:52:28 +0300

    added program interruption hadnling and letter and word counts are measured by amount of typed before ^C or end of text

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <linux/limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -19,14 +20,22 @@ typedef struct colored_symb_array {
     int size;
 } colored_symb_array;
 
+sig_atomic_t running = 1;
+
+void sigint_handler(int s) {
+    running = 0;    
+}
+
 int custom_random(int min, int max) { return min + rand() / (RAND_MAX / (max - min + 1) + 1); }
 
 int render_text(colored_symb_array *text, int width, int height) {
+    printf("\b\b  ");
     printf("\033[H");
     fflush(stdout);
     for (int i = 0; i < text->size; i++) {
         printf("\x1b[%dm%c\x1b[0m", text->array[i].color, text->array[i].symb);
     }
+    printf("\e[?25l");
     fflush(stdout);
     return 0;
 }
@@ -106,17 +115,30 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    sigaction(SIGINT, &(struct sigaction){.sa_handler = sigint_handler, .sa_flags = SA_NOCLDWAIT}, NULL);
+    sigaction(SIGHUP, &(struct sigaction){.sa_handler = sigint_handler, .sa_flags = SA_RESTART}, NULL);
+    sigaction(SIGTERM, &(struct sigaction){.sa_handler = sigint_handler, .sa_flags = SA_RESTART}, NULL);
     char *text = build_text(word_count);
     colored_symb_array *output = build_bash_text(text);
     char symb;
     int curr = 0;
     system("clear");
+
+    int words_count = 0;
+    int letter_count = 0;
+
     time_t start = time(NULL);
-    while (1) {
+    while (running) {
         render_text(output, w.ws_col, w.ws_row);
         symb = getchar();
+        if (symb == -1) {
+            break;
+        }
         if (curr == 0) {
             time_t start = time(NULL);
+        }
+        if (symb == '\0') {
+            break;
         }
         if (symb == '\b' || symb == 127) {
             if (curr == 0) {
@@ -127,11 +149,16 @@ int main(int argc, char **argv) {
             continue;
         }
         if (symb == text[curr]) {
+            letter_count++;
+            if (symb == ' ') {
+                word_count++;
+            }
             set_color(output, 42, curr);
         } else {
             set_color(output, 41, curr);
         }
         if (curr == output->size - 1) {
+            word_count++;
             break;
         }
         curr++;
@@ -140,11 +167,11 @@ int main(int argc, char **argv) {
     double delta = ((double) (int) end - (double) (int) start);
     system("clear");
     printf("delta: %f\n", delta);
-    printf("size: %d\n", output->size);
-    printf("wpm: %f\n", ((double) output->size / 5) * (60 / (double) delta));
-    printf("lpm: %f\n", (double) output->size * (60 / (double) delta));
+    printf("wpm: %f\n", ((double) word_count / 5) * (60 / (double) delta));
+    printf("lpm: %f\n", (double) letter_count * (60 / (double) delta));
     free(output->array);
     free(output);
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    printf("\e[?25h");
     return 0;
 }
